34a4cf18012787d0f73dc5f78c5e063839f2337d
MarkNgaPala <mark.cortes@virtualstaffingph.net>
2024-05-22 02:20:05 +0800

    updated

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\ParentsController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -26,3 +28,4 @@ Route::controller(UserController::class)->group(function(){
     Route::delete('/users/destroy/{user}', 'destroy');
     Route::post('/users/update/{user}', 'update');
 });
+
