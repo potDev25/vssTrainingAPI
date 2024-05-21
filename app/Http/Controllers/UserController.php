2d948e1c19a82bbbe10fb707b2f09afe80edc199
MarkNgaPala <mark.cortes@virtualstaffingph.net>
2024-05-22 00:40:37 +0800

    updated file

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -48,7 +48,7 @@ class UserController extends Controller
     {
         $validated = $request->validate([
             'name' => 'required|string|max:255',
-            'address' => 'required|string|max:255',
+            'email' => 'required|email',
         ]);
 
         
