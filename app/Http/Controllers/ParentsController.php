69448e95186247fc0f4ed52a233aab10a88f7df4
MarkNgaPala <mark.cortes@virtualstaffingph.net>
2024-05-22 00:53:27 +0800

    add controller API

---
+++ app/Http/Controllers/ParentsController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Parents;
+use Illuminate\Http\Request;
+
+class ParentsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Parents $parents)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Parents $parents)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Parents $parents)
+    {
+        //
+    }
+}
